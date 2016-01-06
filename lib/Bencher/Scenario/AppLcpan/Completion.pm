78e0cc4c3cbf0e51e713fcf24889155e9dabd4df
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-06 15:27:46 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/AppLcpan/Completion.pm
@@ -0,0 +1,38 @@
+package Bencher::Scenario::AppLcpan::Completion;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Bencher::ScenarioUtil::Completion qw(make_completion_participant);
+
+our $scenario = {
+    summary => 'Benchmark completion response time, to monitor regression',
+    modules => {
+        'App::lcpan' => 0,
+    },
+    participants => [
+        make_completion_participant(
+            name=>'optname_common_help',
+            cmdline=>"lcpan --hel^",
+        ),
+        make_completion_participant(
+            name=>'optname_common_version',
+            cmdline=>"lcpan --vers^",
+        ),
+        make_completion_participant(
+            name=>'subcmd_author',
+            cmdline=>"lcpan author^",
+        ),
+        make_completion_participant(
+            name=>'optval_author',
+            cmdline=>"lcpan mods --author PERL^",
+        ),
+    ],
+};
+
+1;
+# ABSTRACT:
---
+++ lib/Bencher/Scenarios/AppLcpan.pm
@@ -0,0 +1,11 @@
+package Bencher::Scenarios::App::Lcpan;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: A collection of scenarios to benchmark App::lcpan
+
+=head1 SEE ALSO
+
+L<App::lcpan>
